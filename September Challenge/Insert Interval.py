fddcc1133ea6cd46293ff92dead86df7eca6a84f
Divij <divij.berry1999@gmail.com>
2020-09-14 00:03:35 +0530

    .idea

--- September Challenge/Insert Interval.py
+++ September Challenge/Insert Interval.py
@@ -1,12 +1,13 @@
 class Solution:
-    def insert(self, intervals,newInterval):
+    def insert(self, intervals, newInterval):
         result = []
         i, n = 0, len(intervals)
         while i < n and intervals[i][1] < newInterval[0]:
             result.append(intervals[i])
             i += 1
+        print(result)
 
-        mI = newInterval;
+        mI = newInterval
         while i < n and intervals[i][0] <= newInterval[1]:
             mI[0] = min(mI[0], intervals[i][0])
             mI[1] = max(mI[1], intervals[i][1])
@@ -19,3 +20,8 @@ class Solution:
             i += 1
 
         return result
+
+intervals = [[1,3],[6,9]]
+newInterval = [2,5]
+s = Solution()
+print(s.insert(intervals, newInterval))
