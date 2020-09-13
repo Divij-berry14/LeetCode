49448b07581be9316d6d664fb329cd1dcf1ad2ae
Divij <divij.berry1999@gmail.com>
2020-09-13 23:44:06 +0530

    Combination Sum III.py

--- September Challenge/Insert Interval.py
+++ September Challenge/Insert Interval.py
@@ -1,5 +1,5 @@
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(self, intervals,newInterval):
         result = []
         i, n = 0, len(intervals)
         while i < n and intervals[i][1] < newInterval[0]:
