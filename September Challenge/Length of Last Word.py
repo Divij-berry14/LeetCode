84187c47c24e5cf81db20669ecedbb6d0b809a97
Divij <divij.berry1999@gmail.com>
2020-09-16 00:34:22 +0530

    Length of Last Word.py

---
+++ September Challenge/Length of Last Word.py
@@ -0,0 +1,14 @@
+def lengthofLastWord(s):
+    n = len(s)
+    res = 0
+    while n > 0:
+        n -= 1
+        if s[n] != " ":
+            res += 1
+        elif res > 0:
+            return res
+        else:
+            return res
+
+s = "Helloworld"
+print(lengthofLastWord(s))
