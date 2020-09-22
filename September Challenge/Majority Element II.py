6aa1d5c2d2ab91d769c1427d125d87d69d5b5c47
Divij <divij.berry1999@gmail.com>
2020-09-22 15:55:08 +0530

    Major Element I.py

--- September Challenge/Majority Element II.py
+++ September Challenge/Majority Element II.py
@@ -12,46 +12,8 @@
 # nums = [1,1,1,3,3,2,2,2]
 # print(majorityElement(nums))
 
-# def findCandidate(A):
-#     maj_index = 0
-#     count = 1
-#     for i in range(len(A)):
-#         if A[maj_index] == A[i]:
-#             count += 1
-#             print("1", count)
-#         else:
-#             count -= 1
-#             print("2", count)
-#         if count == 0:
-#             maj_index = i
-#             count = 1
-#             print("3", maj_index, count)
-#     return A[maj_index]
-#
-#
-# # Function to check if the candidate occurs more than n/2 times
-# def isMajority(A, cand):
-#     count = 0
-#     for i in range(len(A)):
-#         if A[i] == cand:
-#             count += 1
-#     if count > len(A) / 3:
-#         return True
-#     else:
-#         return False
-#
-#
-# # Function to print Majority Element
-# def printMajority(A):
-#     # Find the candidate for Majority
-#     cand = findCandidate(A)
-#
-#     # Print the candidate if it is Majority
-#     if isMajority(A, cand) == True:
-#         print("res",cand)
-#     else:
-#         print("No Majority Element")
 
+# Using Moore’s Voting Algorithm)
 def majorityElement(nums):
     if not nums:
         return []
