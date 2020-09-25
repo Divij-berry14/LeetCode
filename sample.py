1b8e74087ea43ecad3b98c73d344f6ef59b85f74
Divij <divij.berry1999@gmail.com>
2020-09-25 23:47:19 +0530

    sample.py

--- sample.py
+++ sample.py
@@ -1,14 +1,14 @@
-# n = input()
-# a = [i+1 for i in range(0, 9)]
-# n1 = list(map(int, str(n)))
-# print(n1)
-# count = 0
+n = input()
+a = [i+1 for i in range(0, 9)]
+n1 = list(map(int, str(n)))
+print(n1)
+count = 0
+for i in range(1,10):
+    n1[i] = n[i]*i
+
+
+# flag = False
 # for i in range(1,10):
-#     n1[i] = n[i]*i
-#
-#
-# # flag = False
-# # for i in range(1,10):
 
 
 
