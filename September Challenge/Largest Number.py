27a2ee49c5fd1235e3217dd4ffa289ab0fb25054
Divij <divij.berry1999@gmail.com>
2020-09-27 23:36:40 +0530

    Largest Number.py

--- September Challenge/Largest Number.py
+++ September Challenge/Largest Number.py
@@ -1,9 +1,15 @@
 class LargerNumKey(str):
     def __lt__(x, y):
+        print(x, y)
         return x + y > y + x
 
 
 class Solution:
     def largestNumber(self, nums):
         largest_num = ''.join(sorted(map(str, nums), key=LargerNumKey))
+        print(largest_num)
         return '0' if largest_num[0] == '0' else largest_num
+
+nums = [3,30,34,5,9]
+m = Solution()
+m.largestNumber(nums)
